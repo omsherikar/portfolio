3bc5dc2ec9f0cec7c546c2b7c644126c1791db29
Om Sherikar <omsherikar0229@gmail.com>
2025-02-14 15:20:48 +0530

    Create musicData.js

---
+++ musicData.js
@@ -0,0 +1,7 @@
+const musicData = [
+  { title: "Tera Mujhse Hai Pehle Ka Naata Koi", src: "/assets/songs/tera-mujhse.mp3" },
+  { title: "Pehli Nazar Mein", src: "/assets/songs/pehli-nazar.mp3" },
+  { title: "Tum Hi Ho", src: "/assets/songs/tum-hi-ho.mp3" },
+];
+
+export default musicData;
