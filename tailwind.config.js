e185fdb8e0baa15f95f7dd579b49079f8de394be
Om Sherikar <omsherikar0229@gmail.com>
2025-02-14 15:13:23 +0530

    Update and rename index.js to tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        lovePink: "#ff758c",
+        loveRed: "#ff3d68",
+        softWhite: "#f9f9f9",
+      },
+    },
+  },
+  plugins: [],
+};
